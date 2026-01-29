605dd40b741554415260ff712aa11f0665f2d301
Evgenii Kudinov <evgenii.kudinov1@gmail.com>
2026-01-29 22:37:13 +0300

    форма логина по центру

--- src/pages/auth/ui/LoginPage.tsx
+++ src/pages/auth/ui/LoginPage.tsx
@@ -1,16 +1,21 @@
 import { Link, useLocation } from 'react-router-dom';
 import { LoginForm } from '@/features/auth/login/ui/LoginForm';
+import cls from './AuthPage.module.css';
 
 export function LoginPage() {
   const location = useLocation();
   const from = (location.state as any)?.from as string | undefined;
 
   return (
-    <div>
-      <h2>Вход</h2>
-      <LoginForm from={from} />
-      <div style={{ marginTop: 12 }}>
-        Нет аккаунта? <Link to="/register" state={{ from }}>Зарегистрироваться</Link>
+    <div className={cls.page}>
+      <div className={cls.card}>
+        <h2 className={cls.title}>Вход</h2>
+
+        <LoginForm from={from} />
+
+        <div className={cls.footer}>
+          Нет аккаунта? <Link to="/register" state={{ from }}>Зарегистрироваться</Link>
+        </div>
       </div>
     </div>
   );
